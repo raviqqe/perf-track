08ce0abbfe6ac5375e5e6deb6706fd9285dc20d0
Yota Toyama <raviqqe@gmail.com>
2022-11-07 18:45:21 -0800

    Fix schema

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS metric (
     id serial PRIMARY KEY,
-    name text UNIQUE
+    name text NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS record (
-    metric_id serial REFERENCES metric (id),
-    commit_hash text,
-    time integer,
+    metric_id serial NOT NULL REFERENCES metric (id) ON DELETE CASCADE,
+    commit_hash text NOT NULL,
+    time integer NOT NULL,
     PRIMARY KEY (metric_id, commit_hash)
 );
