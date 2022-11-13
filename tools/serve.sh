3ed4623752d43ae17d95dfd9f4359f38938b0839
Yota Toyama <raviqqe@gmail.com>
2022-11-13 13:58:26 -0800

    Fix

--- tools/serve.sh
+++ tools/serve.sh
@@ -9,7 +9,7 @@ cd $(dirname $0)/..
 
 mkdir -p tmp
 
-psql $database_host/postgres -c "create database $database_name" || :
+psql $database_host -c "create database $database_name" || :
 psql $database_host/$database_name <migrations/initial.sql
 
 DATABASE_URI=$database_host/$database_name SERVER_ADDRESS=127.0.0.1:8080 ./app
