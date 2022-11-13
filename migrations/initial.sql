9d2121384f2654d847ce572ed16a436748ceba5c
Yota Toyama <raviqqe@gmail.com>
2022-11-13 13:59:08 -0800

    Fix

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS metric (
-    id integer PRIMARY KEY,
+    id uuid PRIMARY KEY,
     name text NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS record (
-    metric_id integer NOT NULL REFERENCES metric (id) ON DELETE CASCADE,
+    metric_id uuid NOT NULL REFERENCES metric (id) ON DELETE CASCADE,
     commit_hash text NOT NULL,
     time integer NOT NULL,
     PRIMARY KEY (metric_id, commit_hash)
