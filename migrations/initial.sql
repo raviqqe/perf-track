b797ce22ed646a16c84f1ace2255e3d0aa956b11
Yota Toyama <raviqqe@gmail.com>
2022-11-13 12:49:54 -0800

    RESTful API implementation (#8)

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS metric (
-    id serial PRIMARY KEY,
+    id integer PRIMARY KEY,
     name text NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS record (
-    metric_id serial NOT NULL REFERENCES metric (id) ON DELETE CASCADE,
+    metric_id integer NOT NULL REFERENCES metric (id) ON DELETE CASCADE,
     commit_hash text NOT NULL,
     time integer NOT NULL,
     PRIMARY KEY (metric_id, commit_hash)
