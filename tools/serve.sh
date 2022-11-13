8996af269e3f68d8b488dcf748e434fa1a156e54
Yota Toyama <raviqqe@gmail.com>
2022-11-13 13:58:09 -0800

    Fix

--- tools/serve.sh
+++ tools/serve.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-database_filename=tmp/app.db
+database_host=postgresql://localhost:5432
+database_name=perf_track
 
 cd $(dirname $0)/..
 
 mkdir -p tmp
 
-sqlite3 $database_filename <migrations/initial.sql
+psql $database_host/postgres -c "create database $database_name" || :
+psql $database_host/$database_name <migrations/initial.sql
 
-DATABASE_URI=sqlite://tmp/app.db SERVER_ADDRESS=127.0.0.1:8080 ./app
+DATABASE_URI=$database_host/$database_name SERVER_ADDRESS=127.0.0.1:8080 ./app
