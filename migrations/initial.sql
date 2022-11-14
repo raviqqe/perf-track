c3e2ab870afdefd7cc0bb2f6f84de28c2797ec3a
Yota Toyama <raviqqe@gmail.com>
2022-11-13 17:35:58 -0800

    Fix

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -1,12 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS metric (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    id text PRIMARY KEY DEFAULT uuid_generate_v4 () ::text,
     name text NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS record (
-    metric_id uuid NOT NULL REFERENCES metric (id) ON DELETE CASCADE,
+    metric_id text NOT NULL REFERENCES metric (id) ON DELETE CASCADE,
     commit_hash text NOT NULL,
     time integer NOT NULL,
     PRIMARY KEY (metric_id, commit_hash)
